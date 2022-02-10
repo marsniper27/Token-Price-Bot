e6063ccef5563d0f3a897763d8cd5ec04414552e
marsniper27 <kyle_marshall_sniper_27@hotmail.com>
2022-02-09 21:29:51 -0400

    remove config

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+//const config = require("./config.json");
 const https = require('https');
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
